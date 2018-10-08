649a0ba0b1407d855cd795a4e3f9ba6c78a4292b
Lu_Cao <zergmk2@gmail.com>
2018-10-08 22:59:39 +0800

    pass 350

---
+++ codeTest/src/main/java/passCode/350.intersection-of-two-arrays-ii.java
@@ -0,0 +1,84 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * [350] Intersection of Two Arrays II
+ *
+ * https://leetcode.com/problems/intersection-of-two-arrays-ii/description/
+ *
+ * algorithms
+ * Easy (44.33%)
+ * Total Accepted:    141.5K
+ * Total Submissions: 314.4K
+ * Testcase Example:  '[1,2,2,1]\n[2,2]'
+ *
+ * Given two arrays, write a function to compute their intersection.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums1 = [1,2,2,1], nums2 = [2,2]
+ * Output: [2,2]
+ * 
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+ * Output: [4,9]
+ * 
+ * 
+ * Note:
+ * 
+ * 
+ * Each element in the result should appear as many times as it shows in both
+ * arrays.
+ * The result can be in any order.
+ * 
+ * 
+ * Follow up:
+ * 
+ * 
+ * What if the given array is already sorted? How would you optimize your
+ * algorithm?
+ * What if nums1's size is small compared to nums2's size? Which algorithm is
+ * better?
+ * What if elements of nums2 are stored on disk, and the memory is limited such
+ * that you cannot load all elements into the memory at once?
+ * 
+ * 
+ */
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1 == null || nums2 == null || nums1.length == 0 || nums2.length == 0)
+        {
+            return new int[0];
+        }
+
+        Map<Integer, Integer> map = new HashMap<>();
+        List<Integer> retList = new ArrayList<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for (int num : nums2) {
+            if (map.containsKey(num) && map.get(num) > 0)
+            {
+                retList.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+
+        int[] retArray = new int[retList.size()];
+        int i = 0;
+        for (int num : retList)
+        {
+            retArray[i++] = num;
+        }
+
+        return retArray;
+    }
+}
