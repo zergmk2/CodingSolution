8df702a634f60b3743220b6bc2665f28882673fe
Lu_Cao <zergmk2@gmail.com>
2018-10-24 00:28:33 +0800

    pass 230

---
+++ codeTest/src/main/java/passCode/tree/230.kth-smallest-element-in-a-bst.java
@@ -0,0 +1,89 @@
+/*
+ * [230] Kth Smallest Element in a BST
+ *
+ * https://leetcode.com/problems/kth-smallest-element-in-a-bst/description/
+ *
+ * algorithms
+ * Medium (47.45%)
+ * Total Accepted:    172.9K
+ * Total Submissions: 361.6K
+ * Testcase Example:  '[3,1,4,null,2]\n1'
+ *
+ * Given a binary search tree, write a function kthSmallest to find the kth
+ * smallest element in it.
+ * 
+ * Note: 
+ * You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: root = [3,1,4,null,2], k = 1
+ * ⁠  3
+ * ⁠ / \
+ * ⁠1   4
+ * ⁠ \
+ * 2
+ * Output: 1
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: root = [5,3,6,2,4,null,null,1], k = 3
+ * ⁠      5
+ * ⁠     / \
+ * ⁠    3   6
+ * ⁠   / \
+ * ⁠  2   4
+ * ⁠ /
+ * ⁠1
+ * Output: 3
+ * 
+ * 
+ * Follow up:
+ * What if the BST is modified (insert/delete operations) often and you need to
+ * find the kth smallest frequently? How would you optimize the kthSmallest
+ * routine?
+ * 
+ */
+
+//import DataStructure.TreeNode;
+
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null || k == 0)
+        {
+            return 0;
+        }
+
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode cur = root;
+        while (cur != null || !stack.isEmpty())
+        {
+            while (cur != null)
+            {
+                stack.push(cur);
+                cur = cur.left;
+            }
+
+            TreeNode node = stack.pop();
+            if (--k == 0)
+            {
+                return node.val;
+            }
+            cur = node.right;
+        }
+        return 0;
+    }
+}
