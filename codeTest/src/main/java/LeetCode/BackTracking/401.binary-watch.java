830cafab55245c47919a8748af05c009f7d48f82
Lu_Cao <zergmk2@gmail.com>
2018-10-27 19:40:17 +0800

    change folder structure

--- codeTest/src/main/java/LeetCode/BackTracking/401.binary-watch.java
+++ codeTest/src/main/java/LeetCode/BackTracking/401.binary-watch.java
@@ -1,3 +1,5 @@
+package LeetCode.BackTracking;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
--- codeTest/src/main/java/LeetCode/BackTracking/78.subsets.java
+++ codeTest/src/main/java/LeetCode/BackTracking/78.subsets.java
@@ -1,3 +1,5 @@
+package LeetCode.BackTracking;
+
 import java.util.ArrayList;
 import java.util.List;
 
