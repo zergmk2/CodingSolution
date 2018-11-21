7236ed32c8c2a45ae790cead42401ae2398ba1b6
Lu_Cao <zergmk2@gmail.com>
2018-11-21 23:14:14 +0800

    pass 303

--- codeTest/src/main/java/Tester.java
+++ codeTest/src/main/java/Tester.java
@@ -1,9 +1,14 @@
 public class Tester {
     public static void main(String[] args) {
         System.out.println("===========");
-        Solution s = new Solution();
-        int[] nums = new int[] {3, 1};
-        int a = s.firstUniqChar("leetcode");
-        System.out.println("===========" + a);
+        int[] nums = new int[] {-2, 0, 3, -5, 2, -1};
+        NumArray n = new NumArray(nums);
+        n.sumRange(0,2);
+        n.sumRange(2,5);
+        n.sumRange(0,5);
+//        Solution s = new Solution();
+//        int[] nums = new int[] {3, 1};
+//        int a = s.("leetcode");
+//        System.out.println("===========" + a);
     }
 }
