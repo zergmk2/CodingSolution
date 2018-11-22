74c84256354aa5b694f59ff1ff25f0195da59285
lu_cao <zergmk2@gmail.com>
2018-11-22 19:47:04 +0800

    add 725

--- codeTest/src/main/java/Tester.java
+++ codeTest/src/main/java/Tester.java
@@ -2,8 +2,8 @@ public class Tester {
     public static void main(String[] args) {
         System.out.println("===========");
         Solution s = new Solution();
-        int[] nums = new int[] {3, 1};
-        int a = s.firstUniqChar("leetcode");
+        int[] nums = new int[] {1,2,3};
+        boolean a = s.isPossible(nums);
         System.out.println("===========" + a);
     }
 }
