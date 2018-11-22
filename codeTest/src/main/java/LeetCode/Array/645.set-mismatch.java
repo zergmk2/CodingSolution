63db8e1c796854de1c5046b8255eec018db98ec8
Lu_Cao <zergmk2@gmail.com>
2018-11-23 00:25:16 +0800

    pass 645

---
+++ codeTest/src/main/java/LeetCode/Array/645.set-mismatch.java
@@ -0,0 +1,61 @@
+/*
+ * [645] Set Mismatch
+ *
+ * https://leetcode.com/problems/set-mismatch/description/
+ *
+ * algorithms
+ * Easy (39.97%)
+ * Total Accepted:    37.4K
+ * Total Submissions: 93.5K
+ * Testcase Example:  '[1,2,2,4]'
+ *
+ * 
+ * The set S originally contains numbers from 1 to n. But unfortunately, due to
+ * the data error, one of the numbers in the set got duplicated to another
+ * number in the set, which results in repetition of one number and loss of
+ * another number. 
+ * 
+ * 
+ * 
+ * Given an array nums representing the data status of this set after the
+ * error. Your task is to firstly find the number occurs twice and then find
+ * the number that is missing. Return them in the form of an array.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: nums = [1,2,2,4]
+ * Output: [2,3]
+ * 
+ * 
+ * 
+ * Note:
+ * 
+ * The given array size will in the range [2, 10000].
+ * The given array's numbers won't have any order.
+ * 
+ * 
+ */
+import java.util.*;
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        if (nums == null || nums.length == 0)
+        {
+            return new int[2];
+        }
+
+        Set<Integer> set = new HashSet<>();
+        int duplicate = 0, n = nums.length;
+        long sum = (n * (n+1)) / 2;
+        for(int i : nums) {
+            if(set.contains(i)) duplicate = i;
+            sum -= i;
+            set.add(i);
+        }
+        return new int[] {duplicate, (int)sum + duplicate};
+        //int assumeSum = nums.size() * ( 2 + (nums.size() - 1)) / 2;
+
+    }
+}
