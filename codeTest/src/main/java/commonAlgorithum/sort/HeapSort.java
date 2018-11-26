d12518b6d5b0bf8e2626db6c99bb9e54dc04c015
Lu_Cao <zergmk2@gmail.com>
2018-11-26 23:46:17 +0800

    add heap sort algorithum

---
+++ codeTest/src/main/java/commonAlgorithum/sort/HeapSort.java
@@ -0,0 +1,82 @@
+package commonAlgorithum.sort;
+import java.util.*;
+
+public class HeapSort
+{
+    private int[] heap;
+    private boolean isMaxHeap;
+
+    public HeapSort(int[] nums, boolean isMaxHeap)
+    {
+        this.isMaxHeap = isMaxHeap;
+        heap = Arrays.copyOf(nums, nums.length);
+        buildHeap();
+    }
+
+    private void buildHeap()
+    {
+        for(int i = (int)Math.floor(this.heap.length/2); i >= 0; i--)
+        {
+            heapify(i, this.heap.length);
+        }
+    }
+
+    private void heapify(int rootIndex, int len)
+    {
+        int left = rootIndex * 2 + 1;
+        int right = rootIndex * 2 + 2;
+        int swapIndex = rootIndex;
+        if (this.isMaxHeap)
+        {
+            if (left < len && heap[left] > heap[swapIndex])
+            {
+                swapIndex = left;
+            }
+
+            if (right < len && heap[right] > heap[swapIndex])
+            {
+                swapIndex = right;
+            }
+        }
+        else
+        {
+            if (left < len && heap[left] < heap[swapIndex])
+            {
+                swapIndex = left;
+            }
+
+            if (right < len && heap[right] < heap[swapIndex])
+            {
+                swapIndex = right;
+            }
+        }
+
+        if (rootIndex != swapIndex)
+        {
+            swap(rootIndex, swapIndex);
+            heapify(swapIndex, len);
+        }
+    }
+
+    private void swap(int a, int b)
+    {
+        int tmp = heap[a];
+        heap[a] = heap[b];
+        heap[b] = tmp;
+    }
+
+    public void sort()
+    {
+        for(int i = heap.length - 1; i >= 0; i--)
+        {
+            swap(i, 0);
+            heapify(0, i - 1);
+        }
+    }
+
+    public int[] getHeapArray()
+    {
+        return this.heap;
+    }
+
+}
