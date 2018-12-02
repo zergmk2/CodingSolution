80ade92e336ce7977d1b63aea52422b6c77ec449
Lu_Cao <zergmk2@gmail.com>
2018-12-02 08:54:48 +0800

    implement part of quick sort.

--- src/main/java/codetest/Algorithum/sort/QuickSort.java
+++ src/main/java/codetest/Algorithum/sort/QuickSort.java
@@ -1,5 +1,46 @@
 package codetest.Algorithum.sort;
 
-public class QuickSort {
+import java.util.ArrayList;
 
+public class QuickSort {
+    public void sort(int[] array)
+    {
+        if (array == null || array.length == 0)
+        {
+            return;
+        }
+        sort(array, 0, array.length - 1);
+    }
+
+    private void sort(int[] array, int l, int r) {
+        if (l < r)
+        {
+            int pivot = partition(array, l, r);
+            sort(array, l, pivot - 1);
+            sort(array, pivot + 1, r);
+        }
+    }
+
+    private int partition(int[] array, int l, int r) {
+        int pivot = array[r];
+
+        int index = l;
+
+        for (int i = l; i < r; i++)
+        {
+            if (array[i] <= pivot)
+            {
+                swap(array, index, i);
+                index++;
+            }
+        }
+    }
+
+    private void swap(int[] array, int a, int b)
+    {
+        int tmp = array[a];
+        array[a] = array[b];
+        array[b] = tmp;
+
+    }
 }
